0dd86bde33925221cefd817e0da720ad04c04c07
takyamamoto <u342691e@ecs.osaka-u.ac.jp>
2021-08-14 20:47:20 +0900

    update

--- contents/_build/jupyter_execute/neural-development/self-organizing-map.jl
+++ contents/_build/jupyter_execute/neural-development/self-organizing-map.jl
@@ -11,9 +11,9 @@ map_width = 10
 w = σw*randn(map_width, map_width, dims) .+ 0.5;
 
 function plot_som(v, w)
-    scatter(v[:, 1], v[:, 2], s=5)
-    scatter(w[:, :, 1], w[:, :, 2], s=5) # w[i, j, 1]とw[i, j, 2]の点をプロット
-    plot(w[:, :, 1], w[:, :, 2], "k"); plot(w[:, :, 1]', w[:, :, 2]', "k")
+    scatter(v[:, 1], v[:, 2], s=10)
+    plot(w[:, :, 1], w[:, :, 2], "k", alpha=0.8); plot(w[:, :, 1]', w[:, :, 2]', "k", alpha=0.8)
+    scatter(w[:, :, 1], w[:, :, 2], s=10) # w[i, j, 1]とw[i, j, 2]の点をプロット
 end;
 
 # Gaussian mask for inputs
@@ -24,38 +24,37 @@ function GaussianMask(sizex=9, sizey=9; σ=5)
     x0, y0 = (sizex-1) / 2, (sizey-1) / 2
     mask = exp.(-((X .- x0) .^2 + (Y .- y0) .^2) / (2.0(σ^2)))
     return mask ./ sum(mask)
-end
+end;
 
-function SOM!(v, w; η0=1.0, σ0=6, T=500)
-    # η0: update rate, σ0 : width, T : training steps
+function SOM!(v, w; α0=1.0, σ0=6, T=500)
+    # α0: update rate, σ0 : width, T : training steps
     map_width = size(w)[1]
     N = size(v)[1]
-    w_history = [copy(w)]
+    w_history = [copy(w)] # history of w
     @showprogress for t in 1:T
-        η = η0 * (1 - t/T) + 0.1; # update rate
+        α = α0 * (1 - t/T) + 0.1; # update rate
         σ = round(Int, (σ0 - 1) * (1 - t/T) + 1); # decay from large to small
-        mask = GaussianMask(2σ+1, 2σ+1, σ=σ);
-        
+        h = GaussianMask(2σ+1, 2σ+1, σ=σ);
         # loop for the N inputs
         for i in 1:N
             dist = sum([(v[i, j] .- w[:, :, j]).^2 for j in 1:dims]) # distance between input and neurons
             win_idx = argmin(dist) # winner index
             idx = [max(1,win_idx[j] - σ):min(map_width, win_idx[j] + σ) for j in 1:2] # neighbor indices
             # update the winner & neighbor neuron
-            α = η * mask[1:length(idx[1]), 1:length(idx[2])]
+            η = α * h[1:length(idx[1]), 1:length(idx[2])]
             for j in 1:dims
-                w[idx..., j] += α .* (v[i, j] .- w[idx..., j])
+                w[idx..., j] += η .* (v[i, j] .- w[idx..., j])
             end
         end
-        append!(w_history, [copy(w)])
+        append!(w_history, [copy(w)]) # save w
     end
     return w_history
 end;
 
-w_history = SOM!(v, w, η0=1.0, σ0=6, T=500);
+w_history = SOM!(v, w, α0=1.0, σ0=6, T=100);
 
 figure(figsize=(6, 4)) 
-idx = [1, 250, 500]
+idx = [1, 50, 100]
 for i in 1:length(idx)
     wh = w_history[idx[i]]
     subplot(2,length(idx),i)
@@ -93,6 +92,8 @@ w += (rand(size(w)...) .- 1) * 0.05;
 w = [w 2l*(rand(M) .- 0.5) hcat(pol2cart.(4π*(rand(M) .- 0.5), r*rand(M))...)']
 w = reshape(w, (map_width, map_width, dims));
 
+SOM!(v, w, α0=1.0, σ0=5, T=50);
+
 function plot_visual_maps(v, w)
     figure(figsize=(8, 8))
     subplot(2,2,1); title("Retinotopic map")
@@ -113,7 +114,5 @@ function plot_visual_maps(v, w)
     contour(X, Y, w[:, :, 3], colors="k", levels=1)
     tight_layout()
 end;
-
-SOM!(v, w, η0=1.0, σ0=5, T=50);
 
 plot_visual_maps(v, w)
