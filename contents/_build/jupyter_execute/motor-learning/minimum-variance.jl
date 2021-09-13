a63b1636c6adb1299c4476ca74b554288d96180b
takyamamoto <u342691e@ecs.osaka-u.ac.jp>
2021-09-14 01:15:57 +0900

    update

--- contents/_build/jupyter_execute/motor-learning/minimum-variance.jl
+++ contents/_build/jupyter_execute/motor-learning/minimum-variance.jl
@@ -11,6 +11,61 @@ function solveEqualityConstrainedQuadProg(P, q, A, b)
     K = [P A'; A zeros(size(A)[1], size(A)[1])] # KKT matrix
     sol = K \ [-q; b]
     return sol[1:size(A)[2]]
+end;
+
+function minimum_variance_model(Ac, Bc, x0, xf, tf, tp, dt)
+    dims = size(x0)[1]
+    ntf = round(Int, tf/dt)
+    ntp = round(Int, tp/dt)
+    nt = ntf + ntp # total time steps    
+    
+    A = LinearAlgebra.I + Ac * dt
+    B = Bc*dt
+    #A = exp(Ac*dt);
+    #B = Ac^-1 * (eye(3) - A) *Bc; 
+    
+    # calculation of V
+    diagV = zeros(nt);
+    for t=0:nt-1
+        if t < ntf
+            diagV[t+1] = sum([(A^(k-t-1) * B * B' * A'^(k-t-1))[1,1] for k=ntf:nt-1])
+        else
+            diagV[t+1] = diagV[t] + (A^(nt-t-2) * B * B' * A'^(nt-t-2))[1,1]
+        end
+    end
+    diagV /= maximum(diagV) # for numerical stability
+    V = Diagonal(diagV); 
+    
+    # 制約条件における行列Cとベクトルdの計算
+    # calculation of C
+    C = zeros(dims*(ntp+1), nt);
+    for p=1:ntp+1
+        for q=1:nt
+            if ntf-1+(p-1)-(q-1) >= 0
+                idx = dims*(p-1)+1:dims*p
+                C[idx, q] = A^(ntf-1-(q-1)+(p-1)) * B # if ntf-1-(q-1)+(p-1) == 0; A^(ntf-1-(q-1)+(p-1))*B equal to B
+            end
+        end
+    end
+    
+    # calculation of d
+    d = vec([xf - A^(ntf+t) * x0 for t=0:ntp]);
+    
+    # 制御信号を二次計画法で計算．
+    # solution by quadratic programming
+    u = solveEqualityConstrainedQuadProg(V, zeros(nt), C, d);
+    
+    # 制御信号を二次計画法で計算．
+    # forward solution
+    x = zeros(dims, nt);
+    x[:,1] = x0;
+    Σ = zeros(dims, dims, nt);
+    Σ[:, :, 1] = B * u[1]^2 * B'
+    for t=1:nt-1
+        x[:,t+1] = A*x[:, t] + B*u[t] # update
+        Σ[:, :, t+1] = A * Σ[:, :, t] * A' + B * u[t]^2 * B' # variance
+    end
+    return x, u, Σ
 end
 
 t1 = 224*1e-3 # time const of eye dynamics (s)
@@ -18,69 +73,33 @@ t2 = 13*1e-3  # another time const of eye dynamics (s)
 tm = 10*1e-3
 dt = 1e-3     # simulation time step (s)
 tf = 50*1e-3  # movement duration (s)
-tp = 30*1e-3  # post-movement duration (s)
-ntf = round(Int, tf/dt)
-ntp = round(Int, tp/dt)
-nt = ntf + ntp # total time steps
+tp = 40*1e-3  # post-movement duration (s)
+nt = round(Int, (tf+tp)/dt) # total time steps
 trange = (1:nt) * dt * 1e3 # ms
 
-x0 = zeros(3)       # initial state (pos=0, vel=0, acc=0)
-xf = [10, 0, 0] # final state (pos=10, vel=0, acc=0)
-α1 = -1/(t1*t2*tm)
-α2 = -1/(t1*t2)-1/(t1*tm)-1/(t2*tm)
-α3 = -1/t1-1/t2-1/tm
-β = 1/tm
-Ac = [0 1 0; 0 0 1; α1 α2 α3];
-Bc = [0, 0, β]
-A = LinearAlgebra.I + Ac * dt
-B = Bc*dt
-#A = exp(Ac*dt);
-#B = Ac^-1 * (eye(3) - A) *Bc; 
+# 2nd order
+x0₂ = zeros(2) # initial state (pos=0, vel=0)
+xf₂ = [10, 0]  # final state (pos=10, vel=0)
+Ac₂ = [0 1; -1/(t1*t2) -1/t1-1/t2];
+Bc₂ = [0, 1]
 
-# calculation of V
-diagV = zeros(nt);
-for i=0:nt-1
-    if i < ntf
-        diagV[i+1] = sum([(A^(k-i-1) * B * B' * A'^(k-i-1))[1,1] for k=ntf:nt-1])
-    else
-        diagV[i+1] = diagV[i] + (A^(nt-i-2) * B * B' * A'^(nt-i-2))[1,1]
-    end
-end
-diagV /= maximum(diagV) # for numerical stability
-V = Diagonal(diagV); 
+# 3rd order
+x0₃ = zeros(3)   # initial state (pos=0, vel=0, acc=0)
+xf₃ = [10, 0, 0] # final state (pos=10, vel=0, acc=0)
+Ac₃ = [0 1 0; 0 0 1; -1/(t1*t2*tm) -1/(t1*t2)-1/(t1*tm)-1/(t2*tm) -1/t1-1/t2-1/tm];
+Bc₃ = [0, 0, 1/tm];
 
-# calculation of C
-C = zeros(3(ntp+1), nt);
-for p=1:ntp+1
-    for q=1:nt
-        if ntf-1+(p-1)-(q-1) >= 0
-            idx = 3(p-1)+1:3p
-            C[idx, q] = A^(ntf-1-(q-1)+(p-1)) * B # if ntf-1-(q-1)+(p-1) == 0; A^(ntf-1-(q-1)+(p-1))*B equal to B
-        end
-    end
-end
+x₂, u₂, Σ₂ = minimum_variance_model(Ac₂, Bc₂, x0₂, xf₂, tf, tp, dt);
+x₃, u₃, Σ₃ = minimum_variance_model(Ac₃, Bc₃, x0₃, xf₃, tf, tp, dt);
 
-# calculation of d
-d = vec([xf - A^(ntf+i) * x0 for i=0:ntp]);
-
-# solution by quadratic programming
-u = solveEqualityConstrainedQuadProg(V, zeros(nt), C, d);
-
-# forward solution
-x = zeros(3, nt);
-x[:,1] = x0;
-for k=1:nt-1
-    x[:,k+1] = A*x[:, k] + B*u[k]
-end
-
-figure(figsize=(10, 3))
-subplot(1,3,1)
-plot(trange, x[1, :])
-ylabel("Eye position (deg)"); xlabel("Time (ms)"); grid()
-subplot(1,3,2)
-plot(trange, x[2, :])
-ylabel("Eye velocity (deg/s)"); xlabel("Time (ms)"); grid()
-subplot(1,3,3)
-plot(trange, u)
-ylabel("Control signal"); xlabel("Time (ms)"); grid()
+figure(figsize=(6, 5))
+subplot(2,2,1); plot(trange, x₂[1, :], label="2nd order"); plot(trange, x₃[1, :], "--", label="3rd order"); 
+ylabel("Eye position (deg)"); grid(); legend()
+subplot(2,2,2); plot(trange, x₂[2, :]); plot(trange, x₃[2, :], "--"); 
+ylabel("Eye velocity (deg/s)"); grid();
+subplot(2,2,3); plot(trange, u₂); plot(trange, u₃, "--");
+ylabel("Control signal"); xlabel("Time (ms)"); grid();
+ax = gca(); ax[:ticklabel_format](style="sci",axis="y",scilimits=(0,0))
+subplot(2,2,4); plot(trange, Σ₂[1,1,:]); plot(trange, Σ₃[1,1,:], "--");
+ylabel("Positional Variance"); xlabel("Time (ms)"); grid()
 tight_layout()
