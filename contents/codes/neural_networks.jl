c1b4f835bdce93b1116e1e36f11c3110c9cdb474
takyamamoto <u342691e@ecs.osaka-u.ac.jp>
2025-02-17 23:11:12 +0900

    update

--- contents/codes/neural_networks.jl
+++ contents/codes/neural_networks.jl
@@ -1,5 +1,8 @@
 using LinearAlgebra, Random
 
+"""
+Activation Function
+"""
 struct ActivationFunction
     forward::Function   # function for forward propagation
     backward::Function  # function for back-propagation
@@ -7,5 +10,221 @@ end
 
 (f::ActivationFunction)(x) = f.forward(x)
 
+function Sigmoid()
+    ActivationFunction(
+        x -> 1.0 ./ (1.0 .+ exp.(-x)),
+        y -> y .* (1 .- y)
+    )
+end
+
+function Tanh()
+    ActivationFunction(
+        x -> tanh.(x),
+        y -> 1 .- y .^ 2
+    )
+end
+
+function ReLU()
+    ActivationFunction(
+        x -> max.(0, x),
+        y -> y .> 0
+    )
+end
+
+function LeakyReLU(a=0.01)
+    ActivationFunction(
+        x -> max.(0, x) + a * min.(0, x),
+        y -> (y .> 0) + a * (y .<= 0)
+    )
+end   
+
+function Identity()
+    ActivationFunction(
+        x -> x, # or use `identity` function
+        y -> fill!(similar(y), 1)
+    )
+end
+
+function softmax(x; dims=2)
+    expx = exp.(x .- maximum(x, dims=dims))
+    return expx ./ sum(expx, dims=dims)
+end
+
+function Softmax()
+    ActivationFunction(softmax, identity)
+end
+
+
+"""
+Neural networks
+"""
 abstract type NeuralNet end
 (f::NeuralNet)(x) = forward!(f, x)
+
+mutable struct Param
+    v::Array
+    grad::Array
+
+    Param(value) = new(value, zero(value))
+end
+
+struct MLP <: NeuralNet
+    L::Int # num. of layers
+    W::Vector{Param}; b::Vector{Param}   # weights and bias
+    z::Vector{Array}; δ::Vector{Array}   # state of forward/backward activity
+    f::Vector{ActivationFunction} # activation functions of layers
+
+    function MLP(n_units::Vector{Int}, f::Vector{ActivationFunction}; init_type="Xavier")
+        L = length(n_units) - 1
+        # initialization of parameters
+        if init_type == "He"
+            W = [Param(2 * (rand(n_units[l], n_units[l+1]) .- 0.5) * sqrt(6/n_units[l])) for l in 1:L] # He
+        else
+            W = [Param(2 * (rand(n_units[l], n_units[l+1]) .- 0.5) * sqrt(6/(n_units[l]+n_units[l+1]))) for l in 1:L] # Xavier
+        end
+        b = [Param(zeros(1, n_units[l+1])) for l in 1:L]
+        
+        # initialization of forward / backward states
+        z, δ = Vector{Array}(undef, L+1), Vector{Array}(undef, L)
+        new(L, W, b, z, δ, f)
+    end
+end
+
+# forward computation of MLP
+function forward!(mlp::MLP, x::Array)
+    (;L, f, W, b, z) = mlp
+    z[1] = x # input (n_batch x n_neurons)
+    for l in 1:L
+        z[l+1] = f[l](z[l] * W[l].v .+ b[l].v) # hidden layers
+    end
+    return z[L+1] # output
+end
+
+function backward!(mlp::MLP)
+    (;L, W, b, z, δ, f) = mlp
+    n_batch = size(z[1])[1]
+    # backprop
+    for l in L:-1:1
+        if l < L
+            δ[l] = δ[l+1] * W[l+1].v' .* f[l].backward(z[l+1])
+        end
+        W[l].grad = z[l]' * δ[l] / n_batch
+        b[l].grad = sum(δ[l], dims=1) / n_batch
+    end
+end
+
+"""
+Loss functions
+"""
+function squared_error!(nn::NeuralNet, y::Array)
+    (;z, δ, f) = nn
+    ŷ = z[end]
+    n_batch = size(y)[1]
+    error = ŷ - y
+    loss = sum(error .^ 2) / n_batch
+    δ[end] = error .* f[end].backward(ŷ)
+    return loss
+end
+
+clog(x) = max(log(x), -1e2) # clamped log
+
+function binary_crossentropy!(nn::NeuralNet, y::Array)
+    (;z, δ, f) = nn
+    @assert f[end] == Sigmoid() # f[end] must be sigmoid function
+    ŷ = z[end]
+    n_batch = size(y)[1]
+    error = ŷ - y
+    loss = -sum(y .* clog.(ŷ) + (1 .- y) .* clog.(1 .- ŷ)) / n_batch
+    δ[end] = error
+    return loss
+end
+
+# t: labels (1 dims)
+function cross_entropy_loss!(nn::NeuralNet, t::Vector)
+    (;z, δ, f) = nn
+    @assert f[end] == Softmax() # f[end] must be softmax function
+    ŷ = z[end]
+    n_batch = length(t)
+    idx = CartesianIndex.([(i, t[i]) for i in 1:n_batch])
+    loss = -sum(clog.(ŷ[idx])) / n_batch
+    grad = copy(ŷ)
+    grad[idx] .-= 1
+    δ[end] = grad / n_batch
+    return loss
+end
+
+# t: probability (2 dims)
+function cross_entropy_loss!(nn::NeuralNet, t::Array)
+    (;z, δ, f) = nn
+    @assert f[end] == Softmax() # f[end] must be softmax function
+    ŷ = z[end]
+    n_batch = size(t)[1]
+    loss = -sum(t .* clog.(ŷ)) / n_batch
+    δ[end] = (ŷ - t) / n_batch
+    return loss
+end
+
+"""
+Optimizer
+"""
+abstract type Optimizer end
+
+# SGD optimizer
+@kwdef struct SGD{FT} <: Optimizer
+    lr::FT=1e-2 # learning rate
+end
+
+# SGD optimizer
+function optimizer_update!(param::Param, optimizer::SGD; weight_decay=0)
+    (;lr) = optimizer
+    if weight_decay > 0
+        param.grad += weight_decay * param.v
+    end
+    param.v -= lr * param.grad
+end
+
+# Adam optimizer
+@kwdef mutable struct Adam <: Optimizer
+    lr=1e-4; β1=0.9; β2=0.999; ϵ=1e-8
+    ms=Dict(); vs=Dict();
+end
+
+# Adam optimizer
+function optimizer_update!(param::Param, optimizer::Adam; weight_decay=0)
+    (;lr, β1, β2, ϵ, ms, vs) = optimizer
+    key = objectid(param)
+    if !haskey(ms, key) 
+        ms[key], vs[key] = zero(param.v), zero(param.v)
+    end
+    if weight_decay > 0
+        param.v -= lr * weight_decay * param.v
+    end
+    m, v = ms[key], vs[key]
+    m += (1 - β1) * (param.grad - m)
+    v += (1 - β2) * (param.grad .* param.grad - v)
+    param.v -= lr * m ./ (sqrt.(v) .+ ϵ)
+end
+
+"""
+Update
+"""
+function optim_step!(nn::NeuralNet, optimizer::Optimizer; weight_decay=0)
+    (;L, W, b) = nn
+    # update weights
+    for param in W
+        optimizer_update!(param, optimizer, weight_decay=weight_decay)
+    end
+
+    # update bias (without weight decay)
+    for param in b
+        optimizer_update!(param, optimizer, weight_decay=0)
+    end
+end
+
+function train_step!(nn::NeuralNet, x::Array, y::Array, loss_fun::Function; optimizer::Optimizer=SGD(), weight_decay=0)
+    _ = nn(x)
+    loss = loss_fun(nn, y)
+    backward!(nn)
+    optim_step!(nn, optimizer, weight_decay=weight_decay) # update params
+    return loss
+end
